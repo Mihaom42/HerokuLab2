d3d74362fe0de04b209cf84a5bdb2196ee412479
Sukharev Alexei <suharev.alexei1@gmail.com>
2023-11-08 14:52:08 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calendar.db'  # Используем SQLite в качестве базы данных
+db = SQLAlchemy(app)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+@app.route('/')
+def index():
+    events = Event.query.all()
+    return render_template('index.html', events=events)
+
+@app.route('/add_event', methods=['POST'])
+def add_event():
+    title = request.form['title']
+    date = datetime.strptime(request.form['date'], '%Y-%m-%d')
+    new_event = Event(title=title, date=date)
+    db.session.add(new_event)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# if __name__ == '__main__':
+#     db.create_all()
+#     app.run(debug=True)
